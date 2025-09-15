linuxdeepin/treeland

feat: impl basic equal-height-grid layout for win switcher
kwin & win11 existing alt+tab switch indicator uses a layout where thumbnails are arranged in rows with equal height, and each with different width according to their h/w ratio.
Qt has no such layout, implemented using colum of rowlayouts & internal calculated layout model

src/treeland/quick/qml/WindowsSwitcher.qml
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 import QtQuick
+import QtQuick.Layouts
 import Waylib.Server
 import TreeLand
 
@@ -14,6 +15,7 @@ Item {
 
     onVisibleChanged: {
         if (visible) {
+            calcLayout()
             next()
         }
         else {
@@ -79,37 +81,147 @@ Item {
                 }
             }
         }
+        // onCountChanged: calcLayout()
+    }
+    // Component.onCompleted: calcLayout()
+    property int spacing: 10
+    property var rows: []
+    property int rowHeight: 0
+    property int padding: 8
+    onRowsChanged: console.log('rows', rows.length, rowHeight)
+    ColumnLayout {
+        id: eqhgrid // equal height grid
+        anchors.centerIn: parent
+        Repeater {
+            model: rows
+            RowLayout {
+                width: eqhgrid.width
+                Layout.alignment: Qt.AlignHCenter
+                property int baseIdx: {
+                    var idx = 0
+                    for (var i = 0; i < index; i++)
+                        idx += rows[i].length
+                    return idx
+                }
+                Repeater {
+                    model: modelData
+                    Rectangle {
+                        property XdgSurface source: modelData.source
+                        width: source.width / source.height * rowHeight + 2 * root.padding
+                        height: col.height + 2 * root.padding
+                        property int globalIdx: index + parent.baseIdx
+                        Component.onCompleted: console.log('item', modelData,
+                                                           index, globalIdx)
+                        border.color: "blue"
+                        border.width: globalIdx == root.current ? 2 : 0
+                        radius: 8
+                        Column {
+                            anchors {
+                                left: parent.left
+                                right: parent.right
+                                top: parent.top
+                                margins: root.padding
+                            }
+                            id: col
+                            RowLayout {
+                                width: parent.width
+                                Rectangle {
+                                    height: width
+                                    width: 24
+                                    color: "yellow"
+                                }
+                                Text {
+                                    Layout.fillWidth: true
+                                    text: "test title ttttttttttttttttttttttt"
+                                    elide: Qt.ElideRight
+                                }
+                            }
+                            Item {
+                                id: thumb
+                                width: parent.width
+                                height: source.height * width / source.width
+                                clip: true
+                                visible: true
+                                ShaderEffectSource {
+                                    anchors.centerIn: parent
+                                    width: parent.width
+                                    height: source.height * width / source.width
+                                    live: true
+                                    hideSource: visible
+                                    smooth: true
+                                    sourceItem: source
+                                }
+                                Component.onCompleted: console.log('thumb',source)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function calcLayout() {
+        console.warn('calcLayout')
+        var minH = 100, maxH = 200
+        function tryLayout(rowH, div) {
+            var nrows = 1
+            var acc = 0
+            var rowstmp = []
+            var currow = []
+            for (var i = 0; i < model.count; i++) {
+                var win = model.get(i)
+                console.log('win',i,'=',win,win.source,win.source.width)
+                var ratio = win.source.width / win.source.height
+                acc += ratio
+                if (acc * rowH <= root.width)
+                    currow.push(win)
+                else {
+                    acc = ratio
+                    nrows++
+                    console.log('cur', currow, 'tmp', rowstmp)
+                    rowstmp.push(currow)
+                    currow = [win]
+                    if (nrows > div)
+                        break
+                }
+                console.info(acc)
+            }
+            if (nrows <= div) {
+                if (currow.length)
+                    rowstmp.push(currow)
+                rowHeight = rowH
+                rows = rowstmp
+                console.log('calcover', nrows, rowH)
+                return true
+            }
+            return false
+        }
+
+        for (var div = 1; root.height / div >= minH; div++) {
+            // return if width satisfies
+            console.log('div=', div)
+            var rowH = Math.min(root.height / div, maxH)
+            if (tryLayout(rowH, div))
+                return
+        }
+        tryLayout(minH, 999)
+        console.warn('cannot layout')
     }
 
     Rectangle {
-        width: switcher.width
-        height: switcher.height
+        width: eqhgrid.width
+        height: eqhgrid.height
         anchors.centerIn: parent
         radius: 10
         opacity: 0.4
     }
 
-    Row {
-        anchors.centerIn: parent
-        id: switcher
-        Repeater {
-            model: model
-            Item {
-                required property XdgSurface source
-                width: 180
-                height: 160
-                clip: true
-                visible: true
-                ShaderEffectSource {
-                    anchors.centerIn: parent
-                    width: 150
-                    height: Math.min(source.height * width / source.width, width)
-                    live: true
-                    hideSource: visible
-                    smooth: true
-                    sourceItem: source
-                }
-            }
-        }
-    }
+    // Row {
+    //     anchors.centerIn: parent
+    //     id: switcher
+    //     Repeater {
+    //         model: model
+    //         
+    //     }
+    // }
 }

SunRain/DeckShell

src/treeland/quick/qml/WindowsSwitcher.qml
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 import QtQuick
+import QtQuick.Layouts
 import Waylib.Server
 import TreeLand
 
@@ -14,6 +15,7 @@ Item {
 
     onVisibleChanged: {
         if (visible) {
+            calcLayout()
             next()
         }
         else {
@@ -79,37 +81,147 @@ Item {
                 }
             }
         }
+        // onCountChanged: calcLayout()
+    }
+    // Component.onCompleted: calcLayout()
+    property int spacing: 10
+    property var rows: []
+    property int rowHeight: 0
+    property int padding: 8
+    onRowsChanged: console.log('rows', rows.length, rowHeight)
+    ColumnLayout {
+        id: eqhgrid // equal height grid
+        anchors.centerIn: parent
+        Repeater {
+            model: rows
+            RowLayout {
+                width: eqhgrid.width
+                Layout.alignment: Qt.AlignHCenter
+                property int baseIdx: {
+                    var idx = 0
+                    for (var i = 0; i < index; i++)
+                        idx += rows[i].length
+                    return idx
+                }
+                Repeater {
+                    model: modelData
+                    Rectangle {
+                        property XdgSurface source: modelData.source
+                        width: source.width / source.height * rowHeight + 2 * root.padding
+                        height: col.height + 2 * root.padding
+                        property int globalIdx: index + parent.baseIdx
+                        Component.onCompleted: console.log('item', modelData,
+                                                           index, globalIdx)
+                        border.color: "blue"
+                        border.width: globalIdx == root.current ? 2 : 0
+                        radius: 8
+                        Column {
+                            anchors {
+                                left: parent.left
+                                right: parent.right
+                                top: parent.top
+                                margins: root.padding
+                            }
+                            id: col
+                            RowLayout {
+                                width: parent.width
+                                Rectangle {
+                                    height: width
+                                    width: 24
+                                    color: "yellow"
+                                }
+                                Text {
+                                    Layout.fillWidth: true
+                                    text: "test title ttttttttttttttttttttttt"
+                                    elide: Qt.ElideRight
+                                }
+                            }
+                            Item {
+                                id: thumb
+                                width: parent.width
+                                height: source.height * width / source.width
+                                clip: true
+                                visible: true
+                                ShaderEffectSource {
+                                    anchors.centerIn: parent
+                                    width: parent.width
+                                    height: source.height * width / source.width
+                                    live: true
+                                    hideSource: visible
+                                    smooth: true
+                                    sourceItem: source
+                                }
+                                Component.onCompleted: console.log('thumb',source)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function calcLayout() {
+        console.log('calcLayout')
+        var minH = 100, maxH = 200
+        function tryLayout(rowH, div) {
+            var nrows = 1
+            var acc = 0
+            var rowstmp = []
+            var currow = []
+            for (var i = 0; i < model.count; i++) {
+                var win = model.get(i)
+                // console.log('win',i,'=',win,win.source,win.source.width)
+                var ratio = win.source.width / win.source.height
+                acc += ratio
+                if (acc * rowH <= root.width)
+                    currow.push(win)
+                else {
+                    acc = ratio
+                    nrows++
+                    // console.log('cur', currow, 'tmp', rowstmp)
+                    rowstmp.push(currow)
+                    currow = [win]
+                    if (nrows > div)
+                        break
+                }
+                console.info(acc)
+            }
+            if (nrows <= div) {
+                if (currow.length)
+                    rowstmp.push(currow)
+                rowHeight = rowH
+                rows = rowstmp
+                // console.log('calcover', nrows, rowH)
+                return true
+            }
+            return false
+        }
+
+        for (var div = 1; root.height / div >= minH; div++) {
+            // return if width satisfies
+            // console.log('div=', div)
+            var rowH = Math.min(root.height / div, maxH)
+            if (tryLayout(rowH, div))
+                return
+        }
+        tryLayout(minH, 999)
+        console.warn('cannot layout')
     }
 
     Rectangle {
-        width: switcher.width
-        height: switcher.height
+        width: eqhgrid.width
+        height: eqhgrid.height
         anchors.centerIn: parent
         radius: 10
         opacity: 0.4
     }
 
-    Row {
-        anchors.centerIn: parent
-        id: switcher
-        Repeater {
-            model: model
-            Item {
-                required property XdgSurface source
-                width: 180
-                height: 160
-                clip: true
-                visible: true
-                ShaderEffectSource {
-                    anchors.centerIn: parent
-                    width: 150
-                    height: Math.min(source.height * width / source.width, width)
-                    live: true
-                    hideSource: visible
-                    smooth: true
-                    sourceItem: source
-                }
-            }
-        }
-    }
+    // Row {
+    //     anchors.centerIn: parent
+    //     id: switcher
+    //     Repeater {
+    //         model: model
+    //         
+    //     }
+    // }
 }
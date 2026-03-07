linuxdeepin/treeland

feat: Add wallpaper producer support for Treeland Wayland compositor
Introduce a Treeland wallpaper management module that enables wallpaper
producers to interact with the Wayland compositor via dedicated unstable
wallpaper protocols.

This change adds server-side implementations for:

treeland-wallpaper-manager-unstable-v1, allowing clients to create and
control per-output wallpaper objects, set image or video sources, and receive
state change notifications.

treeland-wallpaper-shell-unstable-v1, enabling wallpaper producer clients
to create wallpaper surfaces that integrate into the compositor’s rendering
pipeline.

treeland-wallpaper-notifier-unstable-v1, providing broadcast notifications
for wallpaper source additions and removals.

The wallpaper producer is expected to run as a multi-surface Wayland client.
Each wallpaper surface follows the treeland-wallpaper-shell protocol and
provides its content to the compositor as wl_buffer-backed surfaces, allowing
efficient composition and future post-processing support.

Log:
Tasks: https://pms.uniontech.com/story-view-39471.html

Influence:
Adds a new wallpaper module under src/modules, including protocol bindings
and server-side interfaces.

src/modules/wallpaper/wallpapermanagerinterfacev1.h
@@ -1,0 +1,93 @@
+// Copyright (C) 2026 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <wseat.h>
+#include <wsurface.h>
+#include <wserver.h>
+#include <woutput.h>
+
+#include <QObject>
+
+WAYLIB_SERVER_USE_NAMESPACE
+QW_USE_NAMESPACE
+
+class TreelandWallpaperInterfaceV1;
+class TreelandWallpaperManagerInterfaceV1Private;
+class TreelandWallpaperManagerInterfaceV1 : public QObject , public WServerInterface
+{
+    Q_OBJECT
+public:
+    explicit TreelandWallpaperManagerInterfaceV1(QObject *parent = nullptr);
+    ~TreelandWallpaperManagerInterfaceV1() override;
+
+    static constexpr int InterfaceVersion = 1;
+
+Q_SIGNALS:
+    void wallpaperCreated(TreelandWallpaperInterfaceV1 *interface);
+
+protected:
+    void create(WServer *server) override;
+    void destroy(WServer *server) override;
+    wl_global *global() const override;
+    QByteArrayView interfaceName() const override;
+
+private:
+    std::unique_ptr<TreelandWallpaperManagerInterfaceV1Private> d;
+};
+
+class TreelandWallpaperInterfaceV1Private;
+class TreelandWallpaperInterfaceV1 : public QObject
+{
+    Q_OBJECT
+public:
+    enum Error {
+        AlreadyUsed = 0,
+        InvalidSource = 1,
+        PermissionDenied = 2,
+    };
+
+    enum WallpaperType {
+        Image = 0,
+        Video = 1,
+    };
+
+    enum WallpaperRole {
+        Desktop    = 0x1,
+        Lockscreen = 0x2
+    };
+    Q_DECLARE_FLAGS(WallpaperRoles, WallpaperRole)
+    Q_FLAG(WallpaperRoles)
+
+    ~TreelandWallpaperInterfaceV1() override;
+
+    WSurface *referenceWSurface() const;
+    QString userName() const;
+    WOutput *wOutput() const;
+
+    void sendError(const QString &source, Error error);
+    void sendChanged(WallpaperRoles roles, WallpaperType type, const QString &source);
+
+    static TreelandWallpaperInterfaceV1 *getReferenceWallpaperInterfaceFromeSurface(WSurface *surface);
+
+Q_SIGNALS:
+    void imageSourceChanged(int workspaceIndex,
+                            const QString &fileSource,
+                            WallpaperRoles roles);
+    void videoSourceChanged(int workspaceIndex,
+                            const QString &fileSource,
+                            WallpaperRoles roles);
+    void binded();
+
+private:
+    explicit TreelandWallpaperInterfaceV1(struct wl_resource *output,
+                                          const QString &userName,
+                                          wl_resource *resource,
+                                          wl_resource *refsurface,
+                                          QObject *parent = nullptr);
+
+private:
+    friend class TreelandWallpaperManagerInterfaceV1Private;
+    std::unique_ptr<TreelandWallpaperInterfaceV1Private> d;
+};

src/modules/wallpaper/wallpapernotifierinterfacev1.cpp
@@ -1,0 +1,97 @@
+// Copyright (C) 2026 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "wallpapernotifierinterfacev1.h"
+#include "qwayland-server-treeland-wallpaper-shell-unstable-v1.h"
+
+#include <qwdisplay.h>
+
+class TreelandWallpaperNotifierInterfaceV1Private : public QtWaylandServer::treeland_wallpaper_notifier_v1
+{
+public:
+    TreelandWallpaperNotifierInterfaceV1Private(TreelandWallpaperNotifierInterfaceV1 *_q);
+    wl_global *global() const;
+    QList<Resource *> m_resource;
+
+    TreelandWallpaperNotifierInterfaceV1 *q = nullptr;
+
+protected:
+    void treeland_wallpaper_notifier_v1_bind_resource(Resource *resource) override;
+    void treeland_wallpaper_notifier_v1_destroy_resource(Resource *resource) override;
+    void treeland_wallpaper_notifier_v1_destroy_global() override;
+    void treeland_wallpaper_notifier_v1_destroy(Resource *resource) override;
+};
+
+TreelandWallpaperNotifierInterfaceV1Private::TreelandWallpaperNotifierInterfaceV1Private(TreelandWallpaperNotifierInterfaceV1 *_q)
+    : q(_q)
+{
+}
+
+wl_global *TreelandWallpaperNotifierInterfaceV1Private::global() const
+{
+    return m_global;
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_bind_resource(Resource *resource)
+{
+    m_resource.append(resource);
+    Q_EMIT q->binded();
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_destroy_resource(Resource *resource)
+{
+    m_resource.removeOne(resource);
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_destroy_global()
+{
+    m_resource.clear();
+    delete q;
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_destroy(Resource *resource)
+{
+    wl_resource_destroy(resource->handle);
+}
+
+TreelandWallpaperNotifierInterfaceV1::TreelandWallpaperNotifierInterfaceV1(QObject *parent)
+    : QObject(parent)
+    , d(new TreelandWallpaperNotifierInterfaceV1Private(this))
+{
+}
+
+void TreelandWallpaperNotifierInterfaceV1::send_add(TreelandWallpaperInterfaceV1::WallpaperType type, const QString &fileSource)
+{
+    for (auto resource : d->m_resource) {
+        d->send_add(resource->handle, type, fileSource);
+    }
+}
+
+void TreelandWallpaperNotifierInterfaceV1::send_remove(const QString &fileSource)
+{
+    for (auto resource : d->m_resource) {
+        d->send_remove(resource->handle, fileSource);
+    }
+}
+
+TreelandWallpaperNotifierInterfaceV1::~TreelandWallpaperNotifierInterfaceV1() = default;;
+
+void TreelandWallpaperNotifierInterfaceV1::create(WServer *server)
+{
+    d->init(server->handle()->handle(), InterfaceVersion);
+}
+
+void TreelandWallpaperNotifierInterfaceV1::destroy([[maybe_unused]] WServer *server)
+{
+    d = nullptr;
+}
+
+wl_global *TreelandWallpaperNotifierInterfaceV1::global() const
+{
+    return d->global();
+}
+
+QByteArrayView TreelandWallpaperNotifierInterfaceV1::interfaceName() const
+{
+    return d->interfaceName();
+}

src/modules/wallpaper/wallpapernotifierinterfacev1.h
@@ -1,0 +1,42 @@
+// Copyright (C) 2026 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include "wallpapermanagerinterfacev1.h"
+
+#include <wseat.h>
+#include <wsurface.h>
+#include <wserver.h>
+#include <woutput.h>
+
+#include <QObject>
+
+WAYLIB_SERVER_USE_NAMESPACE
+QW_USE_NAMESPACE
+
+class TreelandWallpaperNotifierInterfaceV1Private;
+class TreelandWallpaperNotifierInterfaceV1 : public QObject , public WServerInterface
+{
+    Q_OBJECT
+public:
+    explicit TreelandWallpaperNotifierInterfaceV1(QObject *parent = nullptr);
+    ~TreelandWallpaperNotifierInterfaceV1() override;
+
+    static constexpr int InterfaceVersion = 1;
+
+    void send_add(TreelandWallpaperInterfaceV1::WallpaperType type, const QString &fileSource);
+    void send_remove(const QString &fileSource);
+
+Q_SIGNALS:
+    void binded();
+
+protected:
+    void create(WServer *server) override;
+    void destroy(WServer *server) override;
+    wl_global *global() const override;
+    QByteArrayView interfaceName() const override;
+
+private:
+    std::unique_ptr<TreelandWallpaperNotifierInterfaceV1Private> d;
+};

SunRain/DeckShell

(cherry picked from 9a142df2f9e58ddfae22e3a988d463cfc24bbdc1)
Note: master-merge 修正：modules CMake 调用从 `impl_treeland()` 迁移到 `impl_deckcompositor()`，并添加 `libtreeland` → `libdeckcompositor` ALIAS（保留 `impl_treeland()` wrapper 兼容旧引用）。
Note: Post-merge fix: migrate modules from `impl_treeland()` to `impl_deckcompositor()` and add `libtreeland` → `libdeckcompositor` ALIAS (keep `impl_treeland()` wrapper for compatibility).
Note: master-merge 修正：修正 wallpaper-producer 的源/生成文件路径，使用相对路径与 `${CMAKE_CURRENT_BINARY_DIR}`，避免 out-of-tree build 下路径解析错误。
Note: Post-merge fix: fix wallpaper-producer source/generated paths by using relative paths and `${CMAKE_CURRENT_BINARY_DIR}` to avoid path resolution issues in out-of-tree builds.
Note: master-merge 修正：修正 wallpaper 模块的源文件/生成文件路径，避免使用 ``/`` 在 superbuild 下定位错误，改为相对路径与 ``。
Note: Post-merge fix: fix wallpaper module source/generated paths; avoid ``/`` path issues in superbuild by using relative sources and ``.
(cherry picked from commit 4b081bd84885e8399fd88272bde5fa954ab70e72)

compositor/src/modules/wallpaper/wallpapermanagerinterfacev1.h
@@ -1,0 +1,93 @@
+// Copyright (C) 2026 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <wseat.h>
+#include <wsurface.h>
+#include <wserver.h>
+#include <woutput.h>
+
+#include <QObject>
+
+WAYLIB_SERVER_USE_NAMESPACE
+QW_USE_NAMESPACE
+
+class TreelandWallpaperInterfaceV1;
+class TreelandWallpaperManagerInterfaceV1Private;
+class TreelandWallpaperManagerInterfaceV1 : public QObject , public WServerInterface
+{
+    Q_OBJECT
+public:
+    explicit TreelandWallpaperManagerInterfaceV1(QObject *parent = nullptr);
+    ~TreelandWallpaperManagerInterfaceV1() override;
+
+    static constexpr int InterfaceVersion = 1;
+
+Q_SIGNALS:
+    void wallpaperCreated(TreelandWallpaperInterfaceV1 *interface);
+
+protected:
+    void create(WServer *server) override;
+    void destroy(WServer *server) override;
+    wl_global *global() const override;
+    QByteArrayView interfaceName() const override;
+
+private:
+    std::unique_ptr<TreelandWallpaperManagerInterfaceV1Private> d;
+};
+
+class TreelandWallpaperInterfaceV1Private;
+class TreelandWallpaperInterfaceV1 : public QObject
+{
+    Q_OBJECT
+public:
+    enum Error {
+        AlreadyUsed = 0,
+        InvalidSource = 1,
+        PermissionDenied = 2,
+    };
+
+    enum WallpaperType {
+        Image = 0,
+        Video = 1,
+    };
+
+    enum WallpaperRole {
+        Desktop    = 0x1,
+        Lockscreen = 0x2
+    };
+    Q_DECLARE_FLAGS(WallpaperRoles, WallpaperRole)
+    Q_FLAG(WallpaperRoles)
+
+    ~TreelandWallpaperInterfaceV1() override;
+
+    WSurface *referenceWSurface() const;
+    QString userName() const;
+    WOutput *wOutput() const;
+
+    void sendError(const QString &source, Error error);
+    void sendChanged(WallpaperRoles roles, WallpaperType type, const QString &source);
+
+    static TreelandWallpaperInterfaceV1 *getReferenceWallpaperInterfaceFromSurface(WSurface *surface);
+
+Q_SIGNALS:
+    void imageSourceChanged(int workspaceIndex,
+                            const QString &fileSource,
+                            WallpaperRoles roles);
+    void videoSourceChanged(int workspaceIndex,
+                            const QString &fileSource,
+                            WallpaperRoles roles);
+    void binded();
+
+private:
+    explicit TreelandWallpaperInterfaceV1(struct wl_resource *output,
+                                          const QString &userName,
+                                          wl_resource *resource,
+                                          wl_resource *refsurface,
+                                          QObject *parent = nullptr);
+
+private:
+    friend class TreelandWallpaperManagerInterfaceV1Private;
+    std::unique_ptr<TreelandWallpaperInterfaceV1Private> d;
+};

compositor/src/modules/wallpaper/wallpapernotifierinterfacev1.cpp
@@ -1,0 +1,97 @@
+// Copyright (C) 2026 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "wallpapernotifierinterfacev1.h"
+#include "qwayland-server-treeland-wallpaper-shell-unstable-v1.h"
+
+#include <qwdisplay.h>
+
+class TreelandWallpaperNotifierInterfaceV1Private : public QtWaylandServer::treeland_wallpaper_notifier_v1
+{
+public:
+    TreelandWallpaperNotifierInterfaceV1Private(TreelandWallpaperNotifierInterfaceV1 *_q);
+    wl_global *global() const;
+    QList<Resource *> m_resource;
+
+    TreelandWallpaperNotifierInterfaceV1 *q = nullptr;
+
+protected:
+    void treeland_wallpaper_notifier_v1_bind_resource(Resource *resource) override;
+    void treeland_wallpaper_notifier_v1_destroy_resource(Resource *resource) override;
+    void treeland_wallpaper_notifier_v1_destroy_global() override;
+    void treeland_wallpaper_notifier_v1_destroy(Resource *resource) override;
+};
+
+TreelandWallpaperNotifierInterfaceV1Private::TreelandWallpaperNotifierInterfaceV1Private(TreelandWallpaperNotifierInterfaceV1 *_q)
+    : q(_q)
+{
+}
+
+wl_global *TreelandWallpaperNotifierInterfaceV1Private::global() const
+{
+    return m_global;
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_bind_resource(Resource *resource)
+{
+    m_resource.append(resource);
+    Q_EMIT q->binded();
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_destroy_resource(Resource *resource)
+{
+    m_resource.removeOne(resource);
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_destroy_global()
+{
+    m_resource.clear();
+    delete q;
+}
+
+void TreelandWallpaperNotifierInterfaceV1Private::treeland_wallpaper_notifier_v1_destroy(Resource *resource)
+{
+    wl_resource_destroy(resource->handle);
+}
+
+TreelandWallpaperNotifierInterfaceV1::TreelandWallpaperNotifierInterfaceV1(QObject *parent)
+    : QObject(parent)
+    , d(new TreelandWallpaperNotifierInterfaceV1Private(this))
+{
+}
+
+void TreelandWallpaperNotifierInterfaceV1::sendAdd(TreelandWallpaperInterfaceV1::WallpaperType type, const QString &fileSource)
+{
+    for (auto resource : d->m_resource) {
+        d->send_add(resource->handle, type, fileSource);
+    }
+}
+
+void TreelandWallpaperNotifierInterfaceV1::sendRemove(const QString &fileSource)
+{
+    for (auto resource : d->m_resource) {
+        d->send_remove(resource->handle, fileSource);
+    }
+}
+
+TreelandWallpaperNotifierInterfaceV1::~TreelandWallpaperNotifierInterfaceV1() = default;
+
+void TreelandWallpaperNotifierInterfaceV1::create(WServer *server)
+{
+    d->init(server->handle()->handle(), InterfaceVersion);
+}
+
+void TreelandWallpaperNotifierInterfaceV1::destroy([[maybe_unused]] WServer *server)
+{
+    d = nullptr;
+}
+
+wl_global *TreelandWallpaperNotifierInterfaceV1::global() const
+{
+    return d->global();
+}
+
+QByteArrayView TreelandWallpaperNotifierInterfaceV1::interfaceName() const
+{
+    return d->interfaceName();
+}

compositor/src/modules/wallpaper/wallpapernotifierinterfacev1.h
@@ -1,0 +1,42 @@
+// Copyright (C) 2026 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include "wallpapermanagerinterfacev1.h"
+
+#include <wseat.h>
+#include <wsurface.h>
+#include <wserver.h>
+#include <woutput.h>
+
+#include <QObject>
+
+WAYLIB_SERVER_USE_NAMESPACE
+QW_USE_NAMESPACE
+
+class TreelandWallpaperNotifierInterfaceV1Private;
+class TreelandWallpaperNotifierInterfaceV1 : public QObject , public WServerInterface
+{
+    Q_OBJECT
+public:
+    explicit TreelandWallpaperNotifierInterfaceV1(QObject *parent = nullptr);
+    ~TreelandWallpaperNotifierInterfaceV1() override;
+
+    static constexpr int InterfaceVersion = 1;
+
+    void sendAdd(TreelandWallpaperInterfaceV1::WallpaperType type, const QString &fileSource);
+    void sendRemove(const QString &fileSource);
+
+Q_SIGNALS:
+    void binded();
+
+protected:
+    void create(WServer *server) override;
+    void destroy(WServer *server) override;
+    wl_global *global() const override;
+    QByteArrayView interfaceName() const override;
+
+private:
+    std::unique_ptr<TreelandWallpaperNotifierInterfaceV1Private> d;
+};
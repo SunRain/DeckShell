linuxdeepin/treeland

fix: crash on session removal (logout) window active
when removing the active session, directly setting m_activatedSurface = nullptr
did not clear the active surface history in WorkspaceModel, leading to use-after-free.

Fix by calling workspace()->clearActivedSurface() to clean up all workspace models
(including show-on-all-workspace) before clearing the global activated surface.

Also added missing clearActivedSurface() implementation in WorkspaceModel
and removed unnecessary activatedSurfaceChanged() signal.

Log: fixes logout crash.
PMS: BUG-347763
Influence: logout

src/seat/helper.cpp
@@ -2266,8 +2266,8 @@ void Helper::removeSession(std::shared_ptr<Session> session)
 
     if (m_activeSession.lock() == session) {
         m_activeSession.reset();
-        m_activatedSurface = nullptr;
-        Q_EMIT activatedSurfaceChanged();
+        workspace()->pushActivedSurface();
+        setActivatedSurface(nullptr);
     }
 
     for (auto s : std::as_const(m_sessions)) {
@@ -2466,8 +2466,8 @@ void Helper::updateActiveUserSession(const QString &username, int id)
         // Update active session
         m_activeSession = session;
         // Clear activated surface
+        // TODO: Each Wayland socket's active surface needs to be cleaned up individually.
         setActivatedSurface(nullptr);
-        Q_EMIT activatedSurfaceChanged();
         // Emit signal and update socket enabled state
         if (previous && previous->socket)
             previous->socket->setEnabled(false);

SunRain/DeckShell

(cherry picked from d2de9ca272b43af66039af2cc57f5228c55682d9)
(cherry picked from commit 4548897b901b6582fcd84b2e706645fdef6aa899)

compositor/src/seat/helper.cpp
@@ -2291,8 +2291,8 @@ void Helper::removeSession(std::shared_ptr<Session> session)
 
     if (m_activeSession.lock() == session) {
         m_activeSession.reset();
-        m_activatedSurface = nullptr;
-        Q_EMIT activatedSurfaceChanged();
+        workspace()->clearActivedSurface();
+        setActivatedSurface(nullptr);
     }
 
     for (auto s : std::as_const(m_sessions)) {
@@ -2491,8 +2491,8 @@ void Helper::updateActiveUserSession(const QString &username, int id)
         // Update active session
         m_activeSession = session;
         // Clear activated surface
+        // TODO: Each Wayland socket's active surface needs to be cleaned up individually.
         setActivatedSurface(nullptr);
-        Q_EMIT activatedSurfaceChanged();
         // Emit signal and update socket enabled state
         if (previous && previous->socket)
             previous->socket->setEnabled(false);